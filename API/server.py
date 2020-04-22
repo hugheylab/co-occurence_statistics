1e4529e6afdfbbe1c3c04ecee9894dc196220d1e
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2020-04-21 23:07:26 -0500

    Adds comments to code

--- API/server.py
+++ API/server.py
@@ -6,15 +6,22 @@ import config
 
 connection = db.Connection(config.db["host"], config.db["db"], config.db["user"], config.db["password"])
 
+#General Exposure Endpoint
 @bottle.get('/general_exposure')
 def index():
+    #Assigns parameters to a variable object
     params = bottle.request.query
     results = []
+    #Initializes query to build the string to query for
     query = "SELECT ingredient_id, ingredient_name, general_exposure_count, general_exposure FROM public.general_exposures"
+    #Sets initial limit to the default of 50 results
     limit = "50"
+    #Has a boolean variable to determine if a WHERE clause has been added yet. Is used to determine if in a query WHERE needs to be added or if AND needs to be added
     isWhereAdded = False
+    #Checks for an attribute of limit to specify how many results to limit the query to
     if hasattr(params, "limit") and params.limit != "":
         limit = params.limit
+    #Checks for a parameter of ids, a comma separated list of ingredient Ids to query for
     if hasattr(params, "ids") and params.ids != '':
         isWhereAdded = True
         query = query + " WHERE ("
@@ -26,32 +33,42 @@ def index():
 
         query = query[:-3]
         query = query + ")"
-
+    #Checks for a name parameter to search by ingredient_name in a case insensitive contains method
     if hasattr(params, "name") and params.name != '':
+        #If not isWhereAdded we just add an AND instead of WHERE
         if not isWhereAdded:
             query = query + " WHERE "
         else:
             query = query + " AND "
         nameQuery = params.name.lower()
         query = query + " LOWER(ingredient_name) LIKE '%" + nameQuery + "%' "
-        
+    #Appends limit to query
     query = query + " limit "+limit
+    #Sends query to connection to database
     genExps = connection.read(query)
+    #Returns results, adds them to a list, then puts them in a JSON list
     for ge in genExps:
         results.append(ge)
     return {
         "results": [r for r in results]
         }
+
+#Phenotype Specific Exposures Endpoint
 @bottle.get('/phenotype_specific_exposures')
 def index():
+    #Assigns parameters to a variable object
     params = bottle.request.query
     results = []
+    #Initializes query to build the string to query for
     query = "SELECT ingredient_id, ingredient_name, phecode, phecode_name, paired_count, phecode_cound, phenotype_specific_exposure, enrichment, medi_status FROM public.phenotype_specific_exposures"
+    #Sets initial limit to the default of 50 results
     limit = "50"
+    #Has a boolean variable to determine if a WHERE clause has been added yet. Is used to determine if in a query WHERE needs to be added or if AND needs to be added
     isWhereAdded = False
+    #Checks for an attribute of limit to specify how many results to limit the query to
     if hasattr(params, "limit") and params.limit != "":
         limit = params.limit
-
+    #Checks for a parameter of phecodeIds, a comma separated list of phecodes to query for
     if hasattr(params, "phecodeIds") and params.phecodeIds != '':
         isWhereAdded = True
         query = query + " WHERE ("
@@ -63,7 +80,7 @@ def index():
 
         query = query[:-3]
         query = query + ")"
-
+    #Checks for a parameter of ingredientIds, a comma separated list of ingredient Ids to query for
     if hasattr(params, "ingredientIds") and params.ingredientIds != '':
         if not isWhereAdded:
             query = query + " WHERE ("
